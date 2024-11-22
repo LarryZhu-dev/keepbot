4bb30eef73295a1a81e05be8c9607401ec848e9a
Larry Zhu <larryzhu@foxmail.com>
2024-11-22 17:59:46 +0800

    perf: 更好的发言榜、2b统计

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 import config from './config.js'
 import { pushTalker, getTalkers } from './recorder.js'
 
-const replys = ['2B吧哥们', '2B', '你纯2B', '家人们谁懂啊，有2B艾特我','行了哥们，再发癫就真2B了']
+const replys = ['2B吧哥们', '2B', '你纯2B', '家人们谁懂啊，有2B艾特我', '行了哥们，再发癫就真2B了']
 const followKeyWords = ['2b', '2B', 'sb', 'Sb', 'SB']
 let memberCallCds = []
+let keyWordsTriggerTime = {}
 
 const onMessage = async (message) => {
   const contact = message.talker()
@@ -31,9 +32,13 @@ const onMessage = async (message) => {
       await room.say(talkersRank(topic))
     }
     if (config.groups.includes(topic) && followKeyWords.some((t) => text.includes(t))) {
-      if (!text.startsWith('<msg>') && text.length <= 100 && !text.includes('- - - - - - - - - - -') && memberCallCds.findIndex((item) => item.name === text) == -1) {
-        await room.say(text)
-        memberCallCds.push({ name: text, timer: setTimeout(() => (memberCallCds = memberCallCds.filter((m) => m.name != text)), 6000) })
+      if (!text.startsWith('<msg>') && text.length <= 100 && !text.includes('- - - - - - - - - - -')) {
+        const singleFollowKeyWord = followKeyWords.find((t) => text.includes(t))
+        recordingKeyWords(singleFollowKeyWord)
+        if (memberCallCds.findIndex((item) => item.name === text) == -1) {
+          await room.say(text)
+          memberCallCds.push({ name: text, timer: setTimeout(() => (memberCallCds = memberCallCds.filter((m) => m.name != text)), 1800000) })
+        }
       }
     }
   }
@@ -72,7 +77,31 @@ function talkersRank(topic) {
     })
     .join('\n')
     .toString()
+  const keyWords = getRecordingKeyWords(new Date().toLocaleDateString())
+  if (keyWords) {
+    talkersString += '\n\n一共说了：\n'
+    talkersString += Object.keys(keyWords)
+      .map((k) => {
+        return `${keyWords[k]}个${k}`
+      })
+      .join('、')
+      .toString()
+  }
   return talkersString
 }
-
+function recordingKeyWords(text) {
+  if (!keyWordsTriggerTime[new Date().toLocaleDateString()]) {
+    keyWordsTriggerTime[new Date().toLocaleDateString()] = {}
+  }
+  if (!keyWordsTriggerTime[new Date().toLocaleDateString()][text]) {
+    keyWordsTriggerTime[new Date().toLocaleDateString()][text] = 0
+  }
+  keyWordsTriggerTime[new Date().toLocaleDateString()][text]++
+  if (Object.keys(keyWordsTriggerTime).length > 7) {
+    delete keyWordsTriggerTime[Object.keys(keyWordsTriggerTime)[0]]
+  }
+}
+function getRecordingKeyWords(date) {
+  return keyWordsTriggerTime[date]
+}
 export { onMessage, roomJoin, roomLeave }
